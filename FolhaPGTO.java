6eba8208e31d7dab0b708038b8d1701bfb4355bd
Nicolas <149312021+Shausinaguer@users.noreply.github.com>
2024-11-27 11:34:57 -0300

    (Update) FolhaPGTO.java
    Implementação dos métodos "imprimir" e cálculos resolvidos

--- FolhaPGTO.java
+++ FolhaPGTO.java
@@ -1,11 +1,14 @@
+import java.text.DecimalFormat;
+
 public class FolhaPGTO {
-    
+
     public static void main(String args[]) {
         Mensalista men1, men2;
         Horista hora1, hora2;
 
         // Empregados mensalistas
         men1 = new Mensalista("Jose", "Rua abc", "Junior");
+        men1.calcularIrpf();
         men1.calcularSalario();
         men1.calcularInss();
         imprimir(men1);
@@ -16,22 +19,34 @@ public class FolhaPGTO {
         imprimir(men2);
 
         // Empregados Horistas
-        hora1 = new Horista("Carlos", "Rua xyz",20);
+        hora1 = new Horista("Carlos", "Rua xyz", 20);
         hora1.calcularSalario();
-        System.out.println("Nome: " + hora1.getNome());
-        System.out.println("EnderecoNome: " + hora1.getEndereco());
-        System.out.println("Salario: " + hora1.getSalario());
+        imprimir(hora1);
 
         hora2 = new Horista("Cristina", "Rua do centro", 100);
         hora2.calcularSalario();
-        System.out.println("Nome: " + hora2.getNome());
-        System.out.println("EnderecoNome: " + hora2.getEndereco());
-        System.out.println("Salario: " + hora2.getSalario());
+        imprimir(hora2);
     }
 
-    public static void imprimir(Mensalista x){
+    public static void imprimir(Mensalista x) {
+
+        DecimalFormat deci = new DecimalFormat("0.00");
+
         System.out.println("Nome: " + x.getNome());
         System.out.println("EnderecoNome: " + x.getEndereco());
-        System.out.println("Salario: " + x.getSalario());
+        System.out.println("Salario: " + deci.format(x.getSalario()));
+        System.out.println("IRPF: " + deci.format(x.calcularIrpf()));
+        System.out.println("Inss: " + deci.format(x.calcularInss()) + "\n\n");
+    }
+
+    public static void imprimir(Horista x) {
+
+        DecimalFormat deci = new DecimalFormat("0.00");
+
+        System.out.println("Nome: " + x.getNome());
+        System.out.println("EnderecoNome: " + x.getEndereco());
+        System.out.println("Salario: " + deci.format(x.getSalario()));
+        System.out.println("IRPF: " + deci.format(x.calcularIrpf()));
+        System.out.println("Inss: " + deci.format(x.calcularInss()) + "\n\n");
     }
 }
